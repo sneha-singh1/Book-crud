19627ace2e5a5e0c94804992b46e50c5a6ea82c0
sneha-singh1 <55570248+sneha-singh1@users.noreply.github.com>
2019-10-08 21:35:40 +0530

    Add files via upload

---
+++ src/app/viewcomments/viewcomments.modal.ts
@@ -0,0 +1,10 @@
+// export class View {
+//     Name: string;
+//     Email: string;
+//     Commentu: string;
+//     constructor(Name:string, Email:string, Commentu:string) {
+//         this.Name = Name;
+//         this.Email = Email;
+//         this.Commentu = Commentu;
+//     }
+// }
